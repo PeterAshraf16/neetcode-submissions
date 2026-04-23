b8c25d55accd7e74ed0658fbe4ebb71f7260a667
PeterAshraf16 <121519925+PeterAshraf16@users.noreply.github.com>
2026-04-23 11:45:40 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i< nums.length; i++){
+            if(numbers.contains(nums[i])){
+                return true;
+            }else{
+                numbers.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
